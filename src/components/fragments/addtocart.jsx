a830931cb7347280aff96cd14e21d3ea0f902878
amstrongnpsb <amstrongpsb@gmail.com>
2023-09-24 21:08:01 +0700

    axios

--- src/components/fragments/addtocart.jsx
+++ src/components/fragments/addtocart.jsx
@@ -1,33 +1,8 @@
-import { useState, useEffect } from "react";
-
-function Addtocart({ children, item, products }) {
-  const [totalPrice, setTotalPrice] = useState(0);
-  useEffect(() => {
-    if (item.length > 0) {
-      const total = item.reduce((sum, item) => {
-        const product = products.find((product) => product.id == item.id);
-
-        return sum + product.price * item.qty;
-      }, 0);
-      setTotalPrice(total);
-      localStorage.setItem("cart", JSON.stringify(item));
-    }
-  }, [item, products]);
+function Addtocart({ children }) {
   return (
     <div className="w-2/12  bg-blue-950 text-white text-lg fixed right-0 min-h-[600px] rounded-l-xl flex flex-col items-center font-bold py-5 ">
       Cart List
       {children}
-      {item.length === 0 ? (
-        <p></p>
-      ) : (
-        <div className="font-bold">
-          Total Price :{" "}
-          {totalPrice.toLocaleString("id-ID", {
-            style: "currency",
-            currency: "IDR",
-          })}
-        </div>
-      )}
     </div>
   );
 }
